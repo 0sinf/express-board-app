970e2c4e96a4f8cc5a0e00e34ad20959bc6e89e7
sinf <wjdtp93@gmail.com>
2022-01-04 19:38:30 +0900

    Add hash password

--- services/user.js
+++ services/user.js
@@ -1,7 +1,9 @@
 import { User } from "../models/User.js";
+import bcrypt from "bcrypt";
 
 async function createUser(email, password, name) {
-  const user = await User.createUser(email, password, name);
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const user = await User.createUser(email, hashedPassword, name);
   return user.id;
 }
 
