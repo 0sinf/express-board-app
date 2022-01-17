b9c864ba4a623731ece40f1ed5bfd3a9d55cb036
sinf <wjdtp93@gmail.com>
2022-01-17 21:10:26 +0900

    Fix utils/jwt types

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 
-import loader from "./loader/index.js";
-import logger from "./utils/logger.js";
+import loader from "./loader/index";
+import logger from "./utils/logger";
 dotenv.config();
 
-const port = parseInt(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 3000;
 
 const app = loader();
 
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,21 +1,20 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 import dotenv from "dotenv";
+import { IUser } from "../types/index";
 
 dotenv.config();
-const secretKey = process.env.JWT_SECRET;
+const secretKey: Secret = String(process.env.JWT_SECRET);
 
-export function createToken(user) {
-  const token = jwt.sign(
-    {
-      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7,
-      data: user.id,
-    },
-    secretKey
-  );
+export function createToken(user: IUser) {
+  const jwtPayload: JwtPayload = {
+    exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7,
+    data: user.id,
+  };
+  const token = jwt.sign(jwtPayload, secretKey);
 
   return token;
 }
 
-export function verifyToken(token, cb) {
+export function verifyToken(token: string, cb: () => void) {
   return jwt.verify(token, secretKey, cb);
 }
