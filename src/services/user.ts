b287ffdf12d5d48da46ab8e1d2c6abefd975ba20
sinf <wjdtp93@gmail.com>
2022-01-17 21:16:47 +0900

    Fix services/user typo

--- src/services/user.ts
+++ src/services/user.ts
@@ -3,8 +3,8 @@ import bcrypt from "bcrypt";
 
 async function createUser(email, password, name) {
   const hashedPassword = bcrypt.hashSync(password, 10);
-  const user = await User.createUser(email, hashedPassword, name);
-  return user.id;
+  const userId = await User.createUser(email, hashedPassword, name);
+  return userId;
 }
 
 export { createUser };
