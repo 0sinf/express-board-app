21e4c8f5d2fee880310fb1b76eb96af414c19771
sinf <wjdtp93@gmail.com>
2022-01-25 23:28:58 +0900

    Add user test and Fix user api

--- src/routes/api/user.ts
+++ src/routes/api/user.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import { findUserById } from "../../services/user";
 import asyncHandler from "../middlewares/async-handler";
+import loginRequried from "../middlewares/login-requried";
 
 export default (app: Router) => {
   const route = Router();
 
   route.get(
-    "/:userId",
+    "/",
+    loginRequried,
     asyncHandler(async (req, res) => {
-      const { userId } = req.params;
-      const user = await findUserById(userId);
+      const id = req.user.id;
+      const user = await findUserById(id);
       res.status(200).json(user);
     })
   );
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import post from "./api/post";
 import auth from "./api/auth";
+import user from "./api/user";
 import { Router } from "express";
 
 const app: Router = Router();
@@ -7,5 +8,6 @@ const app: Router = Router();
 export default () => {
   post(app);
   auth(app);
+  user(app);
   return app;
 };
